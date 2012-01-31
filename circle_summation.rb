f0123812f3539b625032bf096b141d649de1a0eb
Curtis Holmes <curtis.holmes@case.edu>
2012-01-30 19:46:15 -0500

    completed circle summation problem

--- circle_summation.rb
+++ circle_summation.rb
@@ -3,31 +3,34 @@
 # Curtis Holmes
 # Interview Street Circle Summation
 
-n = 5
-m = 1
-values = [10,20,30,40,50]
-n_values = [values]*n
+# This function outputs the circle summation
+def circle_summation(n,m,vals)
 
-m.times do |i|
-  n_size = n_values.size 
-  n_size.times do |n|
-    puts n
-    s = n_values[n].size
-    puts n_values[n][i]
-    puts n_values[n][((i-1) % s)] + n_values[n][((i+1) % s)]
-    n_values[n][i] += n_values[n][((i-1) % s)] + n_values[n][((i+1) % s)]
+  n_vals = []
+
+  # Create an array
+  n.times do
+	  n_vals.push vals.clone
   end
+
+  # Loop through each function
+  m.times do |j|
+	  n.times do |i|
+		  n_vals[i][((i+j)% n)] += n_vals[i][((i+j-1)% n)] + n_vals[i][((i+j+1)% n)]
+	  end
+  end
+
+  #print out each value
+  n_vals.each { |val| val.each { |n| print (n%1000000007).to_s + " " }; print "\n" }
+  print "\n" 
 end
 
-puts "******"
-puts n_values
+# This gets the number of test cases
+tests = gets.chomp
 
-
-# This gets the input and returns an output
-
-#tests = gets.chomp
-
-#tests.to_i.times do
-#  line = gets.chomp
-#  puts sim(line, line) 
-#end
+# This gets each input and puts it into the circle summation function
+tests.to_i.times do
+  n_m = gets.chomp
+  values = gets.chomp
+  circle_summation(n_m.split(" ")[0].to_i, n_m.split(" ")[1].to_i, values.split(" ").map { |s| s.to_i }) 
+end
