af6827ecd836d26542a2a9e087e5d073ab5ed6a7
Curtis Holmes <curtis.holmes@case.edu>
2012-02-04 20:18:11 -0500

    Added new challenge

--- string_similarity.rb
+++ string_similarity.rb
@@ -4,6 +4,9 @@
 # Interview Street String Similarity
 
 # This function has two counters, left and right, and a value
+# The right and left compare, and if they are the same, they both move right. When the pattern is broken,
+# the left goes back to 0 and the right goes to the next from where it started. Each match, and value increases.
+# Also, if right is at the end of the string, the pattern is maxed out and it goes back to where it was + 1
 def sim(str)
     left = 0; right = 1; value = str.length
     
